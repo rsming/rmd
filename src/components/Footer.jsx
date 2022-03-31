3a4fb10038c6e11473d775847dbd1688576bf547
rsming <rsming@gmail.com>
2022-03-31 13:50:34 +1100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
-const Footer = () => {
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
     return (
-    <div className="">
-      FOOTER
-    </div>
-  )
+      <footer className="footer has-background-white-ter">
+        <div className="columns">
+
+          <div className="column is-4">
+          </div>
+
+          <div className="column is-4 has-text-centered">
+            <h4 className="bd-footer-title">Copyright &copy; 2022. All rights reserved.</h4>
+          </div>
+          
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
